b54670191f97eb4b43745f6d83c511045be87a43
Sage Farrenholz <sagefarrenholz@sages-macbook-air.local>
2019-01-13 18:46:04 -0500

    Initial Commit

---
+++ opengl-test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  opengl-test
+//
+//  Created by Sage Farrenholz on 1/13/19.
+//  Copyright © 2019 Sage Farrenholz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
